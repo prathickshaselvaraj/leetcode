e331ed9022a926625239e4e0914dc70ac84d4907
PRATHICKSHA.S. <148404681+prathickshaselvaraj@users.noreply.github.com>
2026-03-23 09:33:16 +0530

    Time: 0 ms (100%), Space: 12.5 MB (91.22%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res=[0]*len(nums)
+        for i in range(len(nums)):
+            res[i]=nums[nums[i]]
+        return res
+        
